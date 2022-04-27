391f8cd379b45ceb2783274f75440ea1c8ab1b9d
Morgan Atwood <atwood.morganj@gmail.com>
2022-04-26 21:52:23 -0700

    Passport local strategy init

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dtos/createUser.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -12,10 +14,20 @@ export class AuthController {
     return user;
   }
 
+  //@UseGuards(AuthGuard('local'))
+  @UseGuards(LocalAuthGuard)
   @Post('/signin')
   async signIn(@Body() body: CreateUserDto) {
-    const user = await this.authService.signUserIn(body.email, body.password);
+    const token = await this.authService.signUserIn(body.email, body.password);
+    const user = { ...token };
 
     return user;
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('/login')
+  async login(@Request() req) {
+    console.log(req);
+    return req.user;
   }
 }
