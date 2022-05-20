d1f778d5fdc5b2ae2e08f9bb5cc759bd84898989
Morgan Atwood <atwood.morganj@gmail.com>
2022-05-19 21:07:45 -0700

    Postgres init

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,18 @@ import { AppService } from './app.service';
 import { User } from './user/user.entity';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { Connection } from 'typeorm';
 
 @Module({
   imports: [
     UserModule,
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'sqlite.test.db',
+      type: 'postgres',
+      database: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'password123',
       synchronize: true,
       entities: [User],
     }),
@@ -31,4 +36,31 @@ import { AuthModule } from './auth/auth.module';
     },
   ],
 })
-export class AppModule {}
+
+// @Module({
+//   imports: [
+//     UserModule,
+//     TypeOrmModule.forRoot({
+//       type: 'sqlite',
+//       database: 'sqlite.test.db',
+//       synchronize: true,
+//       entities: [User],
+//     }),
+//     AuthModule,
+//   ],
+//   controllers: [AppController],
+//   providers: [
+//     AppService,
+//     {
+//       provide: APP_PIPE,
+//       useValue: new ValidationPipe({
+//         whitelist: true,
+//         forbidNonWhitelisted: true,
+//         transform: true,
+//       }),
+//     },
+//   ],
+// })
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
